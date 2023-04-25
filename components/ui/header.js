c47ec218dbec47a1caa6d60b418d379c6ffa7d61
j0hannr <johann.rohn@gmail.com>
2023-04-25 19:56:21 +0200

    😕 Without Emojis

--- components/ui/header.js
+++ components/ui/header.js
@@ -2,53 +2,53 @@ import { createElement } from "react";
 import { motion } from "framer-motion";
 import { IoMegaphoneOutline, IoCalendarOutline, IoChatbubbleOutline, IoHeartOutline  } from "react-icons/io5";
 import React from "react";
-import Emoji from "react-apple-emojis";
+// import Emoji from "react-apple-emojis";
 import Umfragen from "../content/slides/umfragen";
 
 const renderHTML = (rawHTML) => createElement("span", { dangerouslySetInnerHTML: { __html: rawHTML } });
 
 export default function Header({contentType, content}) {
-    const emoji = require("emoji-dictionary");
-    let emojiName = emoji.getName(content.emoji);
+    // const emoji = require("emoji-dictionary");
+    // let emojiName = emoji.getName(content.emoji);
     
-    if (emojiName) {  
+    // if (emojiName) {  
 
-        // replace all underscores with dashes until there are no underscores left
-        while (emojiName.includes("_")) {
-            emojiName = emojiName.replace("_", "-");
-        }
+    //     // replace all underscores with dashes until there are no underscores left
+    //     while (emojiName.includes("_")) {
+    //         emojiName = emojiName.replace("_", "-");
+    //     }
 
-        // replace false names with correct ones
-        if (emojiName=="iphone") {
-            emojiName = "mobile-phone";
-        }
+    //     // replace false names with correct ones
+    //     if (emojiName=="iphone") {
+    //         emojiName = "mobile-phone";
+    //     }
 
-        if (emojiName=="heart") {
-            emojiName = "red-heart";
-        }
+    //     if (emojiName=="heart") {
+    //         emojiName = "red-heart";
+    //     }
         
-        if (emojiName=="+1") {
-            emojiName = "thumbs-up";
-        }
+    //     if (emojiName=="+1") {
+    //         emojiName = "thumbs-up";
+    //     }
 
-        if (emojiName=="loud-sound") {
-            emojiName = "speaker-high-volume";
-        }
+    //     if (emojiName=="loud-sound") {
+    //         emojiName = "speaker-high-volume";
+    //     }
 
-        if (emojiName=="eyeglasses") {
-            emojiName = "glasses";
-        }
+    //     if (emojiName=="eyeglasses") {
+    //         emojiName = "glasses";
+    //     }
 
-        if (emojiName=="busstop") {
-            emojiName = "bus-stop";
-        }
-        if (emojiName=="bulb") {
-            emojiName = "light-bulb";
-        }
-        if (emojiName=="speak-no-evil") {
-            emojiName = "speak-no-evil-monkey";
-        }
-    }
+    //     if (emojiName=="busstop") {
+    //         emojiName = "bus-stop";
+    //     }
+    //     if (emojiName=="bulb") {
+    //         emojiName = "light-bulb";
+    //     }
+    //     if (emojiName=="speak-no-evil") {
+    //         emojiName = "speak-no-evil-monkey";
+    //     }
+    // }
 
 
 
@@ -101,7 +101,7 @@ export default function Header({contentType, content}) {
                         <div className="font-bold ml-auto max-w-[660px] overflow-hidden text-ellipsis">
                             {content?.project && renderHTML(content.project)}
                         </div>
-                        {emojiName && <Emoji className="ml-3" name={emojiName} width={38} />}
+                        {/* {emojiName && <Emoji className="ml-3" name={emojiName} width={38} />} */}
                     </>
                 )}
             </div>
